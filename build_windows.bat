16f3e91b099c1e0890bc7c402294a2605e346929
Rich Guilmain <richguilmain@gmail.com>
2014-12-30 00:18:57 -0500

    Refactored build_windows.bat a bit.

--- build_windows.bat
+++ build_windows.bat
@@ -1,8 +1,5 @@
 mkdir Tacktx
-third_party\7zip\7za.exe a Tacktx\Tacktx.zip .\src\*
-cd Tacktx
-rename Tacktx.zip Tacktx.love
-cd ..
+third_party\7zip\7za.exe a -tzip Tacktx\Tacktx.love .\src\*
 copy /b third_party\love\love.exe+Tacktx\Tacktx.love Tacktx\Tacktx.exe
 erase Tacktx\Tacktx.love
 xcopy /y third_party\love\*.dll Tacktx
