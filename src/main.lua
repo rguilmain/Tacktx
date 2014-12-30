d2a68fce421d53e48371fa400a08b68ce914eb7c
Rich Guilmain <richguilmain@gmail.com>
2014-12-30 00:55:03 -0500

    Using local keyword where appropriate.
    For loop control variables in Lua are local by default.
    We should also probably employ some naming convention to denote globals.

--- src/main.lua
+++ src/main.lua
@@ -2,7 +2,7 @@ function love.load()
     love.window.setTitle("Tacktx")
 
     -- Load our hex images.
-    tileset = "tilesets/flat_left_right/"
+    local tileset = "tilesets/flat_left_right/"
     sheep_hex = love.graphics.newImage(tileset .. "sheep.gif")
 
     -- h is the distance (in pixels) from the bottom of the tile image to the
@@ -17,8 +17,8 @@ function love.draw()
     for col = 0, map_num_cols - 1 do
         for row = 0, map_num_rows - 1 do
             -- If we're in an odd row, offset the x value by half a tile width.
-            x = (col + (row % 2) * 0.5) * sheep_hex:getWidth()
-            y = row * (sheep_hex:getHeight() - tile_h)
+            local x = (col + (row % 2) * 0.5) * sheep_hex:getWidth()
+            local y = row * (sheep_hex:getHeight() - tile_h)
             love.graphics.draw(sheep_hex, x, y)
         end
     end
