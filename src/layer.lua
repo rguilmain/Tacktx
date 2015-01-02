768b3a767e1b16cc251ebb63255b6249673b327a
Thomas DeRensis <tderensis@gmail.com>
2015-01-02 01:10:49 -0500

    Added a function to calculate the row and column that was clicked. Draw a circle there for now just as a visual indication.

--- src/layer.lua
+++ src/layer.lua
@@ -20,12 +20,59 @@ function HexTileLayer:draw()
     for icol,index in ipairs(row) do
       if index > 0 then
         -- If we're in an odd row, offset the x value by half a tile width.
-        local x = (icol - (irow % 2) * 0.5) * self.tile_width
-        local y = irow * (self.tile_height - offset)
+        local x = ((icol - 1) + ((irow - 1) % 2) * 0.5) * self.tile_width
+        local y = (irow - 1) * (self.tile_height - offset)
         love.graphics.draw(self.tile_images[index], x, y)
       end
     end
   end
+end
+
+function HexTileLayer:getTileIndexFromPoint(x, y)
+   -- first assume we are in the center rectangle region of the hex tile
+   -- TODO: remove magic numbers
+  local row = math.floor(y/48) + 1
+  if row%2 == 1 then
+    col = math.floor(x/55) + 1
+  else
+    col = math.floor((x-(55/2))/55) + 1
+  end
+
+  local in_center = (y%48 >= 17)
+
+  if not in_center then
+    local q = math.floor(x/(55/2)) + 1
+    if (row%2 == 1 and q%2 == 1) or (row%2 == 0 and q%2 == 0) then
+      -- use cross product
+      local x1 = math.floor((q-1)*55/2)
+      local y1 = row*48 - 32
+      local x2 = x1 + (55/2)
+      local y2 = y1 - 16
+      local v1 = {x2-x1, y2-y1}
+      local v2 = {x2-x, y2-y}
+      local xp = v1[1]*v2[2] - v1[2]*v2[1]
+      -- we are actually on the row above
+      if xp > 0 then
+        if row%2 == 1 then col = col - 1 end
+        row = row - 1
+      end
+    else
+      -- use cross product
+      local x1 = math.floor((q-1)*55/2)
+      local y1 = (row-1)*48
+      local x2 = x1 + (55/2)
+      local y2 = y1 + 16
+      local v1 = {x2-x1, y2-y1}
+      local v2 = {x2-x, y2-y}
+      local xp = v1[1]*v2[2] - v1[2]*v2[1]
+      -- we are actually on the row above
+      if xp > 0 then
+        if row%2 == 0 then col = col + 1 end
+        row = row - 1
+      end
+    end
+  end
+  return row, col
 end
 
 return HexTileLayer
--- src/main.lua
+++ src/main.lua
@@ -8,8 +8,28 @@ function love.load()
   love.window.setMode(1000, 750, {resizable=true})
 
   map = require 'layers.middle_earth'
+
+  bx = 0
+  by = 0
 end
 
 function love.draw()
-  map:draw()
+   map:draw()
+   love.graphics.setColor(255,255,255)
+   love.graphics.circle("fill", bx, by, 20, 20)
+end
+
+function love.mousereleased(x, y, button)
+   if button == 'l' then
+     row, col = map:getTileIndexFromPoint(x, y)
+     print("At: " .. row .. ", " .. col)
+
+     -- set tile center, hard coded for now
+     by = row*48 - 16
+     if row%2 == 1 then
+        bx = col*55 - 55/2
+     else
+        bx = col*55
+     end
+  end
 end
