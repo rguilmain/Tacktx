c12aca933cae6ae691247d256a48befa7622dd0e
Rich Guilmain <richguilmain@gmail.com>
2015-01-03 15:34:13 -0500

    Add right clicking logic so we can move TO places!

--- src/main.lua
+++ src/main.lua
@@ -9,21 +9,40 @@ function love.load()
 
   map = require 'layers.middle_earth'
 
-  bx = 0
-  by = 0
+  x_left = 0
+  y_left = 0
+  left_button_clicked = false
+
+  x_right = 0
+  y_right = 0
+  right_button_clicked = false
 end
 
 function love.draw()
-  map:draw()
   love.graphics.setColor(255,255,255)
-  love.graphics.circle("fill", bx, by, 20, 20)
+  map:draw()
+  if left_button_clicked then
+     love.graphics.setColor(255,255,255)
+     love.graphics.circle("fill", x_left, y_left, 20, 20)
+  end
+  if right_button_clicked then
+     love.graphics.setColor(255,0,255)
+     love.graphics.circle("fill", x_right, y_right, 20, 20)
+  end
 end
 
 function love.mousereleased(x, y, button)
+  row, col = map:getTileIndexFromPoint(x, y)
+  print("At: " .. row .. ", " .. col)
   if button == 'l' then
-    row, col = map:getTileIndexFromPoint(x, y)
-    print("At: " .. row .. ", " .. col)
-
-    bx, by = map:getTileCenterFromIndex(row, col)
+     x_left, y_left = map:getTileCenterFromIndex(row, col)
+     left_button_clicked = true
+     right_button_clicked = false
+  elseif button == 'r' then
+     if not left_button_clicked then
+        return
+     end
+     x_right, y_right = map:getTileCenterFromIndex(row, col)
+     right_button_clicked = true
   end
 end
