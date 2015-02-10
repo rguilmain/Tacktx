95280fc02a10ef67d82cac6f958fea64bd59147b
Rich Guilmain <richguilmain@gmail.com>
2015-02-09 20:38:50 -0500

    Move third_party to build/third_party.

--- build/build_windows.bat
+++ build/build_windows.bat
@@ -1,6 +1,6 @@
 mkdir dist
-..\third_party\7zip\7za.exe a -tzip dist\Tacktx.love ..\src\*
-copy /b ..\third_party\love\love.exe+dist\Tacktx.love dist\Tacktx.exe
+third_party\7zip\7za.exe a -tzip dist\Tacktx.love ..\src\*
+copy /b third_party\love\love.exe+dist\Tacktx.love dist\Tacktx.exe
 erase dist\Tacktx.love
-xcopy /y ..\third_party\love\*.dll dist
-xcopy /y ..\third_party\love\license.txt dist
+xcopy /y third_party\love\*.dll dist
+xcopy /y third_party\love\license.txt dist
